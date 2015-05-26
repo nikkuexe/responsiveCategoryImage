8de03ae07b0dc33a8e372fdbf024e8a0e3a10ff5
Nicholas Graham <nicholas@Nicholass-MacBook-Pro.local>
2015-05-26 16:23:02 +0900

    fixed plugin key bug

--- class.responsiveCategoryImage_vanilla.plugin.php
+++ class.responsiveCategoryImage_vanilla.plugin.php
@@ -2,7 +2,7 @@
 // Modified from categoryimageheader by VrijVlinder, with derived code from @peregrine.
 
 
-$PluginInfo['responsiveCategoryImage_vanilla'] = array(
+$PluginInfo['responsiveCategoryImage'] = array(
     'Name' => 'Responsive Category Image',
     'Description' => 'Adds a responsive image to the top of category pages.',
     'Version' => '0.9',
@@ -15,7 +15,7 @@ $PluginInfo['responsiveCategoryImage_vanilla'] = array(
 class responsiveCategoryImage_vanilla extends Gdn_Plugin {
 
    public function Base_Render_Before($Sender) {
-      $Sender->AddCssFile($this->GetResource('design/responsiveCategoryImage_vanilla.css', FALSE, FALSE));
+      $Sender->AddCssFile($this->GetResource('design/responsiveCategoryImage.css', FALSE, FALSE));
    }
  
    public function CategoriesController_BeforeRenderAsset_Handler($Sender) {
